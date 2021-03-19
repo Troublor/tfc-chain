a8895b64ce97ba93bc0c7b414b2fbc6445f43e68
Wuqi Aaron Zhang <troublor@live.com>
2021-03-19 18:44:17 +0800

    :construction: move sector from fnode to rnode

--- packages/tfc-chain-core/contracts/RNode.sol
+++ packages/tfc-chain-core/contracts/RNode.sol
@@ -5,4 +5,50 @@ import "./interfaces.sol";
 contract RNode {
     string id;
     IFNode[] fnodes; // a list of fnodes
+
+    Sector[] rewardedSectors;
+    Sector[] unrewardedSectors;
+
+    struct Sector {
+        address submitter; // the account that mines this sector
+        string id; // the id of this sector
+        string afid; // the afid of this sector
+        string merkleRoot; // the merkle root of this sector
+        Seed[] rewardedSeeds;
+        Seed[] unrewardedSeeds;
+    }
+
+    struct Seed {
+        address submitter; // the account who takes this photo as seed
+        address[] rewardedVerifiers; // the list of accounts who have verified the seed
+        address[] unrewardedVerifiers; // the list of accounts who have verified the seed
+        string afid; // afid of this photo
+        uint256 timestamp; // timestamp of this photo
+        address rnodeOwner; // the account who generate the sector corresponding to this seed
+        string sectorId; // the id of the sector
+        string sectorAfid; // the afid of the sector
+        string merkleRoot; // the merkle root of the sector
+    }
+
+    function submitSector(string id, string afid, string merkleRoot) public {
+        Sector sector;
+        // TODO may need to check msg.sender is human
+        sector.submitter = msg.sender;
+        sector.id = id;
+        sector.afid = afid;
+        sector.merkleRoot = merkleRoot;
+        unrewardedSectors.push(sector);
+    }
+
+    function submitSeed(string afid, uint256 timestamp, address rnodeOwner, string rnodeId, string sectorId, string sectorAfid, string merkleRoot) public {
+        Seed seed;
+        // TODO may need to check msg.sender is human
+        seed.submitter = msg.sender;
+        seed.afid = afid;
+        seed.timestamp = timestamp;
+        seed.rnodeOwner = rnodeOwner;
+        seed.sectorId = sectorId;
+        seed.sectorAfid = sectorAfid;
+        seed.merkleRoot = merkleRoot;
+    }
 }
