7ef259b56761bddf0fb5f60031f82780cb05b8e4
Wuqi Aaron Zhang <troublor@live.com>
2021-03-24 21:47:24 +0800

    :white_check_mark: add/update test

--- packages/tfc-chain-core/test/turbofil.test.ts
+++ packages/tfc-chain-core/test/turbofil.test.ts
@@ -2,6 +2,7 @@ import {ethers} from 'hardhat';
 import {TurboFil, TurboFil__factory} from '../typechain';
 import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/signers';
 import {ContractReceipt} from 'ethers';
+import {fail} from 'assert';
 
 describe('TurboFil Contract', () => {
     let vault: SignerWithAddress;
@@ -32,11 +33,26 @@ describe('TurboFil Contract', () => {
         await expect(promise).rejects.toThrow('TurboFil: Unregistered account');
     });
 
-    function expectEvent(receipt: ContractReceipt, parameters: Record<string, unknown>): void {
-        expect(receipt.events?.length).toBeGreaterThanOrEqual(1);
-        const event = receipt.events?.[0].args;
-        for (const key of Object.keys(parameters)) {
-            expect(event).toHaveProperty(key, parameters[key]);
+    function expectEvent(receipt: ContractReceipt, events: Record<string, Record<string, unknown>>): void {
+        if (!receipt.events) {
+            fail('no events emitted in the receipt');
+            return;
+        }
+        expect(receipt.events?.length).toBeGreaterThanOrEqual(Object.keys(events).length);
+        for (const eventName of Object.keys(events)) {
+            const matched = receipt.events.some(ev => {
+                for (const key of Object.keys(events[eventName])) {
+                    if (!ev.args || !ev.args[key] || ev.args[key] !== events[eventName][key]) {
+                        return false;
+                    }
+                }
+                return true;
+            });
+            if (!matched) {
+                fail(`event ${eventName}(${Object.keys(events[eventName])
+                    .map(key => `${key}:${events[eventName][key]}`)
+                    .join(', ')}) is not found in receipt`);
+            }
         }
     }
 
@@ -44,10 +60,12 @@ describe('TurboFil Contract', () => {
         // use mainAccount
         turboFil = turboFil.connect(mainAccount);
         const tx = await turboFil.register(vault.address);
-        const receipt = await tx.wait(0);
+        const receipt = await tx.wait();
         expectEvent(receipt, {
-            addr: mainAccount.address,
-            recommender: vault.address,
+            Register: {
+                addr: mainAccount.address,
+                recommender: vault.address,
+            },
         });
     });
 
@@ -55,13 +73,15 @@ describe('TurboFil Contract', () => {
         // use mainAccount
         turboFil = turboFil.connect(mainAccount);
         let tx = await turboFil.register(vault.address);
-        await tx.wait(0);
+        await tx.wait();
         turboFil = turboFil.connect(otherAccount);
         tx = await turboFil.register(mainAccount.address);
-        const receipt = await tx.wait(0);
+        const receipt = await tx.wait();
         expectEvent(receipt, {
-            addr: otherAccount.address,
-            recommender: mainAccount.address,
+            Register: {
+                addr: otherAccount.address,
+                recommender: mainAccount.address,
+            },
         });
     });
 
@@ -75,7 +95,46 @@ describe('TurboFil Contract', () => {
         // use mainAccount
         turboFil = turboFil.connect(mainAccount);
         const tx = await turboFil.register(vault.address);
-        await tx.wait(0);
+        await tx.wait();
         await expect(turboFil.register(vault.address)).rejects.toThrow('TurboFil: Already-registered account');
+    });
+
+    test('should allow a registered user to register Mobile/RNode/FNode', async () => {
+        // use mainAccount
+        turboFil = turboFil.connect(mainAccount);
+        let tx = await turboFil.register(vault.address);
+        await tx.wait();
+
+        // register mobile
+        tx = await turboFil.registerMobile('+1 23456');
+        let receipt = await tx.wait();
+        expectEvent(receipt, {
+            RegisterMobile: {
+                addr: mainAccount.address,
+                phone: '+1 23456',
+            },
+        });
+
+        // register rnode
+        tx = await turboFil.registerRNode('rnode');
+        receipt = await tx.wait();
+        expectEvent(receipt, {
+            RegisterRNode: {
+                addr: mainAccount.address,
+                id: 'rnode',
+            },
+        });
+        const rnodeAddress = receipt.events?.[0].args?.['rnode'];
+
+        // register mobile
+        tx = await turboFil['registerFNode(string,string)']('fnode', 'rnode');
+        receipt = await tx.wait();
+        expectEvent(receipt, {
+            RegisterFNode: {
+                addr: mainAccount.address,
+                id: 'fnode',
+                rnode: rnodeAddress,
+            },
+        });
     });
 });
